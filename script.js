52caf9d1ef5021e9da9375485cbaec6c39ee2e10
acav310 <acavanaugh2013@gmail.com>
2022-03-28 09:41:45 -0400

    functionality1

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+function myclick(target) {
+  target.innerText = "Logout";
+}
+
+// switch back to "Login" ??
+
+// ------ accept/deny request --------------------------------------------
+const invite = document.querySelector('.phil')
+
+function hide(invite) {
+  invite.remove();
+}
+
+const inviteb = document.querySelector('.todd')
+
+function hide(inviteb) {
+  inviteb.remove();
+
+  // edit number of requests and connections upon click
+}// --------------------------------------------------------
